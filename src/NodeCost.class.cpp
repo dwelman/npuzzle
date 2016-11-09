37d632f7014bc48ed95db664edb69dd8a06a8b2e
Dean DU TOIT <ddu-toit@e5r7p3.jnb.42.fr>
2016-11-09 10:41:55 +0200

    a

--- src/NodeCost.class.cpp
+++ src/NodeCost.class.cpp
@@ -20,29 +20,14 @@ void	Node::setCost()
 
 int     Node::manhattanCostTile(int tile)
 {
-    int startX;
-    int startY;
-    int endX;
-    int endY;
     int deltaX;
     int deltaY;
-    int cost;
 
-    startX = this->getX(tile);
-    startY = this->getY(tile);        
-    endX = this->finalState->getX(tile);
-    endY = this->finalState->getY(tile);
-/*    cout << endl << "tile : " <<tile <<endl;
-    cout << "start : (" << startX << " ; " << startY << ")" << endl;     
-    cout << "end : (" << endX << " ; " << endY << ")" <<endl;
-*/  deltaX = endX - startX;
-    deltaY = endY - startY;
-    if (deltaX < 0)
-        deltaX *= -1;
-    if (deltaY < 0)
-        deltaY *= -1;
-    cost = deltaX + deltaY;        
-    return (cost);
+    deltaX = this->finalState->getX(tile) - this->getX(tile);
+    deltaY = this->finalState->getY(tile) - this->getY(tile);
+    deltaX = (deltaX < 0) ? deltaX *= -1 : deltaX;
+    deltaY = (deltaY < 0) ? deltaY *= -1 : deltaY;  
+    return (deltaX + deltaY);
 }
 
 void	Node::manhattanCost(void)
@@ -71,11 +56,37 @@ void	Node::manhattanCost(void)
         }
         y++;
     }
-    //cout << "----------------------------" << endl << "Total Cost :" << totalCost << endl;
+    cout << "----------------------------" << endl << "Total Cost :" << totalCost << endl;
 }
 
 void	Node::euclidianCost(void)
 {
+    int x;
+    int y;
+    int cost;
+    int totalCost;
+
+    x = 0;
+    y = 0;
+    totalCost = 0;
+    while (y < this->size)
+    {
+        x = 0;
+        while (x < this->size)
+        {
+            if (this->tiles[y][x] == 0)
+            {
+                x++;
+                continue ;
+            }
+            cost = manhattanCostTile(this->tiles[y][x]);
+            totalCost += cost;
+            x++;
+        }
+        y++;
+    }
+    setCostToGoal(totalCost);
+    cout << "----------------------------" << endl << "Total Cost :" << totalCost << endl;
 }
 
 void	Node::toopCost(void)
